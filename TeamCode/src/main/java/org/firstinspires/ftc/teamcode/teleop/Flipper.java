fc173d69460c9b9bd687deedf41fd049e7108bf6
Surabhi <surabhisharma7676@gmail.com>
2020-01-04 18:01:14 -0800

    Commented Flipper.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Flipper.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Flipper.java
@@ -6,6 +6,12 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.teleop.Robot;
 
+/*
+ * The Flipper class is used to push stones off the robot and onto the foundation
+ * A back and forth action is used to push the stones
+ * The MIN and MAX position are needed for the FLIPPER to function
+ * The FLIPPER moves by alternating between its MIN and MAX position
+ */ 
 public class Flipper {
 
 
@@ -13,11 +19,13 @@ public class Flipper {
     private Robot cBot;
     private OpMode opMode;
 
-    final double MIN_FLIPPER_POWER = 0;
-    final double MAX_FLIPPER_POWER = 1;
+    final double MIN_FLIPPER_POWER = 0; //the FLIPPER'S minimum position (0)
+    final double MAX_FLIPPER_POWER = 1; //the FLIPPER'S maximum position (1)
 
-
-    private double servoPower = MIN_FLIPPER_POWER; //Initialize the FLIPPER to the starting position
+    /*
+     * Initialize the FLIPPER to the starting position
+     */
+    private double servoPower = MIN_FLIPPER_POWER;
 
     /*
      * Constructor
@@ -31,10 +39,17 @@ public class Flipper {
         raise();
 
     }
-public void raise(){
 
+    /*
+     * Raises the FLIPPER and sets it to its maximum position (1)
+     */
+public void raise(){
         cBot.flipper.setPosition(MAX_FLIPPER_POWER);
 }
+
+/*
+ * Lowers the FLIPPER and sets it to its minimum position (0)
+ */
 public void lower(){
 
     cBot.flipper.setPosition(MIN_FLIPPER_POWER);
